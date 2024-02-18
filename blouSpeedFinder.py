1daa76f6cf9e7ba82e76a85eeeb86656b0fc641c
abertell <59854865+abertell@users.noreply.github.com>
2024-02-18 05:52:45 -0500

    Update blouSpeedFinder.py

--- blouSpeedFinder.py
+++ blouSpeedFinder.py
@@ -3,23 +3,27 @@
 
 from math import trunc, floor
 
+playerW = 13.8
+tileW = 25
+eps = 0.01
+
 def rnd(x):
     return trunc(x*20)/20
 
 def bloublou(x,v,wall,left=False):
-    if left: return floor((x+v-13.8/2+1)/25)*25>=wall and rnd(x+v)<wall+5.89
-    return floor((x+v+13.8/2-1)/25)*25<wall and rnd(x+v)>wall-5.91
+    if left: return floor((x+v-playerW/2+1)/tileW)*tileW >= wall and rnd(x+v) < wall+playerW/2-1-eps
+    return floor((x+v+playerW/2-1)/tileW)*tileW < wall and rnd(x+v) > wall-playerW/2+1-eps
 
 def testblou(wall):
     speeds = []
     for v in range(-470,471):
-        w = wall*20+(118 if v<0 else -118)
+        w = wall*20 + (118 if v<0 else -118)
         nx = (w-v)/20
         if bloublou(nx,v/20,wall,v<0): speeds.append(v/20)
     return speeds
 
 def DispBlouSpeeds(wallX):
-    print(f'{wallX}:')
+    print(f'x = {wallX}:')
     speeds = testblou(wallX)
     i = 0
     while i<len(speeds):
